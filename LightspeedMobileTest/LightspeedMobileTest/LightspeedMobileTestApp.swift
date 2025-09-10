f7c4e13cd55cf2809291c6d6af0cfb3e3c4ecd06
mike011 <mike_ch_1@hotmail.com>
2025-09-10 14:48:40 -0400

    Project Creation

---
+++ LightspeedMobileTest/LightspeedMobileTest/LightspeedMobileTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightspeedMobileTestApp.swift
+//  LightspeedMobileTest
+//
+//  Created by Michael Charland on 2025-09-10.
+//
+
+import SwiftUI
+
+@main
+struct LightspeedMobileTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
